0ab2f324f6b77c5c11aea08a9ad4d7e6022c7cf1
Siddhartha Dhar Choudhury <38374913+frankhart2018@users.noreply.github.com>
2019-01-04 22:28:06 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = mysqli_connect("shareddb1e.hosting.stackcp.net","vastukosh-32331f64","password98@","vastukosh-32331f64");
+    $link = mysqli_connect(********);
     if(isset($_GET['email']) && !empty($_GET['email'])) {
         $query = "UPDATE `users` SET status=1 WHERE email ='".mysqli_real_escape_string($link, $_GET['email'])."'";
         mysqli_query($link, $query);
