2578b32777c24c26fc8501c3d817582a444c7025
Siddhartha Dhar Choudhury <38374913+frankhart2018@users.noreply.github.com>
2019-01-04 22:27:18 +0530

    Update rent_and_sell_form.php

--- rent_and_sell_form.php
+++ rent_and_sell_form.php
@@ -2,7 +2,7 @@
 
     session_start();
 
-    $link = mysqli_connect("shareddb1e.hosting.stackcp.net","vastukosh-32331f64","password98@","vastukosh-32331f64");
+    $link = mysqli_connect(**********);
 
     if(isset($_POST['lsubmit'])) {
         if(isset($_POST['iid']) && !empty($_POST['iid']) && $_POST['ichoice']!='1' && isset($_POST['price']) && !empty($_POST['price'])) {
